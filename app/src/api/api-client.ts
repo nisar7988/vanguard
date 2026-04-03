d7ed1036258cab6ea591d929ebb0d9a667062400
Nisar Ahmed <your-correct-github-email>
2026-04-03 12:13:11 +0530

    deployed BE to render

--- app/src/api/api-client.ts
+++ app/src/api/api-client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_Response_Error } from "../types/api-response";
 import { useAuthStore } from "../store/use-auth-store";
 
-const API_BASE_URL = "http://192.168.1.173:3000/api";
+const API_BASE_URL = "https://vanguard-twsr.onrender.com/api";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
